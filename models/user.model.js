7878c7efecbdc8bf8270b18d6a3012da7675329b
Md Mohibbullah <mohammedhanif74612@gmail.com>
2026-08-23 21:16:50 +0300

    feat: user's schema created successfully & docs

---
+++ models/user.model.js
@@ -0,0 +1,56 @@
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    role: {
+      type: String,
+      required: true,
+    },
+    avater: {
+      url: {
+        type: String,
+        required: true,
+      },
+      public_id: {
+        type: String,
+        required: true,
+      },
+    },
+  },
+  { timestamps: true },
+);
+
+userSchema.pre("save", async (next) => {
+  if (this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isCorrectPassword = async (password) => {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.accessToken = async () => {
+  return jwt.sign({ id: this._id }, process.env.JWT_TOKEN_SECRET, {
+    expiresIn: process.env.JWT_TOKEN_EXPIRETED,
+  });
+};
+
+export const User = mongoose.models("User", userSchema);
